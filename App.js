419d1ea041e6dafb1a86eed24c0da3caf3ff0d31
Sameem-Amjad <sameemamjadarsu@gmail.com>
2023-12-16 16:27:58 +0500

    Change to Material Top Navigator

--- App.js
+++ App.js
@@ -1,174 +1,19 @@
-import React, { useState } from 'react';
-import {
-  StatusBar,
-  Text,
-  View,
-  StyleSheet,
-  TextInput,
-  TouchableOpacity,
-  FlatList,
-  Keyboard,
-  ToastAndroid,
-  TouchableHighlight,
-  Pressable,
-} from 'react-native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import ToDOInScrollView from './To_do_Screens/ToDoInScroolView';
+import ToDoInFlatList from './To_do_Screens/ToDOInFlatList';
+import {NavigationContainer} from '@react-navigation/native';
+import ToDoInAsyncStorage from './To_do_Screens/ToDOInAsyncStorage';
+const Tab = createMaterialTopTabNavigator();
+
 function App() {
-  const [item, setItem] = useState('');
-
-  const [list, setList] = useState([]);
-  const [edit, setEdit] = useState(-1);
-  const addItem = () => {
-    if (item !== '') {
-      setList([...list, {data: item, id: Math.random()}]);
-    }
-    setItem('');
-    Keyboard.dismiss(true);
-    ToastAndroid.showWithGravityAndOffset(
-      'Touch the Text item for Update',
-      ToastAndroid.SHORT,
-      ToastAndroid.CENTER,
-      40,
-      10,
-    );
-  };
-  const deleteItem = itemList => {
-    setList(
-      list.filter(item => {
-        return item !== itemList;
-      }),
-    );
-    setEdit(-1);
-    setItem('');
-  };
-  const updateItem = itemList => {
-    setEdit(itemList.id);
-    setItem(itemList.data);
-  };
-  const updateList = () => {
-    setList(
-      list.map(element =>
-        element.id === edit ? (element = {data: item, id: edit}) : element,
-      ),
-    );
-    setEdit(-1);
-    setItem('');
-  };
-
   return (
-    <View>
-      <StatusBar backgroundColor="skyblue" barStyle="dark-content" />
-      <View style={style.mainView}>
-        <Text style={style.mainViewText}>TO-DO List sameem</Text>
-      </View>
-      <View style={style.mainInputView}>
-        <View style={style.inputView}>
-          <TextInput
-            onChangeText={setItem}
-            placeholder="Enter an Item"
-            value={item}
-            style={style.textInput}></TextInput>
-          <TouchableHighlight
-            activeOpacity={0.6}
-            underlayColor="grey"
-            style={style.button}
-            onPress={edit === -1 ? addItem : updateList}>
-            <Text
-              style={{textAlign: 'center', color: 'white', fontWeight: 'bold'}}>
-              {edit === -1 ? `Submit` : `Update`}
-            </Text>
-          </TouchableHighlight>
-        </View>
-      </View>
-      <View style={{alignItems: 'center'}}>
-        <FlatList
-          data={list}
-          renderItem={itemList => (
-            <TouchableOpacity
-              style={style.listView}
-              onPress={() => updateItem(itemList)}
-              key={itemList.id}>
-              <Text
-                style={{
-                  textAlign: 'center',
-                  fontSize: 20,
-                  fontWeight: 'bold',
-                  color: 'white',
-                  width: '60%',
-                }}>
-                {itemList.data}
-              </Text>
-              <View style>
-                <Pressable
-                  style={{
-                    padding: '8%',
-                  }}>
-                  <Text
-                    style={{
-                      textAlign: 'center',
-                      color: 'white',
-                      fontSize: 20,
-                    }}
-                    onPress={() => deleteItem(itemList)}>
-                    🗑
-                  </Text>
-                </Pressable>
-              </View>
-            </TouchableOpacity>
-          )}
-          keyExtractor={itemList => itemList.id}
-        />
-      </View>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Async Storage" component={ ToDoInAsyncStorage } />
+        <Tab.Screen name='Scroll View' component={ToDOInScrollView}/>
+        <Tab.Screen name="FlatList" component={ToDoInFlatList} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 export default App;
-const style = StyleSheet.create({
-  mainView: {
-    backgroundColor: 'skyblue',
-    padding: 10,
-  },
-  mainViewText: {
-    textAlign: 'center',
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-  mainInputView: {
-    margin: 5,
-    alignItems: 'center',
-  },
-  inputView: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    width: '80%',
-    justifyContent: 'space-around',
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: 'skyblue',
-    padding: 2,
-    width: '70%',
-    margin: 2,
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: 'skyblue',
-    borderRadius: 5,
-  },
-  button: {
-    backgroundColor: 'skyblue',
-    padding: 7,
-    borderRadius: 5,
-  },
-  listView: {
-    borderWidth: 2,
-    margin: 6,
-    padding: 4,
-    alignItems: 'center',
-    backgroundColor: 'skyblue',
-    borderColor: 'skyblue',
-    flexDirection: 'row',
-    width: '75%',
-    borderRadius: 9,
-    justifyContent: 'space-around',
-  },
-});
